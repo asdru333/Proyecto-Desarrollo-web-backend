0e5c8d6126aca874818357265c8415f336229825
asdru333 <asdrubal.villegas.molina@hotmail.com>
2022-07-06 12:55:53 -0600

    descripcion a postres agregada

--- pizza_magma_backend/controllers/deserts.js
+++ pizza_magma_backend/controllers/deserts.js
@@ -5,18 +5,21 @@ exports.listDeserts = async (req, res) => {
   const deserts = [{
     id: 1,
     name: 'Helado de vainilla',
+    description: '3 bolas de helado que se puede pedir con sirope de chocolate o caramelo',
     price: 2100,
     image: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/02/comida-basura-2242805.jpg'
   },
   {
     id: 2,
     name: 'Tres leches',
+    description: 'Podés pedir una pieza o el queque entero',
     price: 2900,
     image: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/02/comida-basura-2242805.jpg'
   },
   {
     id: 3,
     name: 'flan',
+    description: 'Podés pedir una pieza o el queque entero',
     price: 3300,
     image: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/02/comida-basura-2242805.jpg'
   }
