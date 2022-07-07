c909614ef7eea28568881ea1d79d13e096b9f476
asdru333 <asdrubal.villegas.molina@hotmail.com>
2022-07-06 20:15:50 -0600

    Agregado imágenes a los elementos que faltaban

--- pizza_magma_backend/controllers/deserts.js
+++ pizza_magma_backend/controllers/deserts.js
@@ -6,21 +6,21 @@ exports.listDeserts = async (req, res) => {
     name: 'Helado de vainilla',
     description: '3 bolas de helado que se puede pedir con sirope de chocolate o caramelo',
     price: 2100,
-    image: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/02/comida-basura-2242805.jpg'
+    image: 'https://ci0137.s3.amazonaws.com/magma/postres/helado_vainilla.png'
   },
   {
     id: 2,
     name: 'Tres leches',
     description: 'Pedazo de Tres leches, disfruta el sabor costarricense',
     price: 2900,
-    image: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/02/comida-basura-2242805.jpg'
+    image: 'https://ci0137.s3.amazonaws.com/magma/postres/tres_leches.png'
   },
   {
     id: 3,
     name: 'flan',
     description: 'Disfrutá del flan más exquisito',
     price: 3300,
-    image: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/02/comida-basura-2242805.jpg'
+    image: 'https://ci0137.s3.amazonaws.com/magma/postres/flan.png'
   }
 ]
   res.json(deserts);
